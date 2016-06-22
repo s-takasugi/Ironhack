8fd71b909b56098e2efd26721fc4757bc4debb48
Soichiro Takasugi <soichiro.takasugi@gmail.com>
2016-06-22 08:54:53 +0200

    edit todolist

--- week2/day1/sinatra_todo/todolist.rb
+++ week2/day1/sinatra_todo/todolist.rb
@@ -1,10 +1,12 @@
 # Week2 Day1 Exercise <Sinatra Todo>
 
 require './task'
+require 'yaml/store'
 
 class TodoList
 	attr_reader :tasks, :user
 	def initialize(user)
+		@todo_store = YAML::Store.new("./public/tasks.yml")
 		@tasks = []
 		@user = user
 	end
@@ -24,18 +26,26 @@ class TodoList
 	def sort_by_created
 		@tasks.sort{ |a, b| b.created_at <=> a.created_at}
 	end
+
+	def save
+		@todo_store.transaction do
+			@todo_store[@user] = @tasks
+		end
+	end
 end
 
 
 
-# todolist = TodoList.new("Soy")
-# task = Task.new("Walk the dog")
-# task2 = Task.new("Buy the milk")
+todo_list = TodoList.new("Soy")
+task = Task.new("Walk the dog")
+task2 = Task.new("Buy the milk")
+task3 = Task.new("Make my todo list into a web app")
+todo_list.save
+
+# todo_list = TodoList.new("Soy")
+# todo_list.add_task
+puts todo_list.tasks.length
 # todolist.add_task(task)
 # todolist.add_task(task2)
-
+#
 # p todolist.sort_by_created
-
-
-
-
