0902df2d781ab6c0e875ad4f816f12a6f49aba81
Soichiro Takasugi <soichiro.takasugi@gmail.com>
2016-06-29 22:57:07 +0200

    complete iteration#4

--- week3/day3/class_works/caesar_ceipher/encrypt.js
+++ week3/day3/class_works/caesar_ceipher/encrypt.js
@@ -1,19 +1,42 @@
-function caesarCipher (message) {
+function caesarCipher (message, shift) {
   var message = message.split('');
+  shift = shift || -3
 
-  message = message.map(function(letter, i){
-    if ((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z')){
-    return letter.charCodeAt(0) - 3
-  }else {
-    return letter.charCodeAt(0);
-  }});
-  message = message.map(function(num){
-    return String.fromCharCode(num)
+  message = message.map(function(letter) {
+    if (letter >= 'A' && letter <= 'Z') {
+      letter = letter.charCodeAt(0) + shift; {
+        if (letter < 65) {
+          return letter + 26
+        } else if (letter > 90) {
+            return letter -26
+          } else {
+              return letter;
+          }
+          return letter;
+        };
+        return letter
+    } else if (letter >= 'a' && letter <= 'z') {
+        letter = letter.charCodeAt(0) + shift;
+        if (letter < 97) {
+          return letter + 26
+        } else if (letter >122) {
+            return letter -26
+          } else {
+              return letter;
+          }
+          return letter;
+      } else {
+          return letter.charCodeAt(0);
+        };
+      return message
   });
-  message = message.reduce(function(sum, i){
-    return sum + i
+  message = message.map(function(num) {
+      return String.fromCharCode(num);
+  });
+  message = message.reduce(function(sum, i) {
+    return sum + i;
   });
   return message;
 }
-var encrypted = caesarCipher ('Oh, my G@D!!!')
+var encrypted = caesarCipher ('Et tu brute?', -6);
 console.log(encrypted);
