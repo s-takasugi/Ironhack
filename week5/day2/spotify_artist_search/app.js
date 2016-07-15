807bec4741092a408d42aa0b5b648c9ad4b9a91e
Soichiro Takasugi <soichiro.takasugi@gmail.com>
2016-07-15 10:51:31 +0200

    first commit (week5/day2)

--- week5/day2/spotify_artist_search/app.js
+++ week5/day2/spotify_artist_search/app.js
@@ -1,12 +1,13 @@
 $(function () {
     $('.form').on('submit', function(e) {
         e.preventDefault();
+        $('#list').empty()
         var user_input = $('#searchbox').val()
         console.log(user_input);
         $.ajax({
             type: 'GET',
             url: 'https://api.spotify.com/v1/search?type=artist&query=' + user_input,
-            success: handleResponse,
+            success: artistController,
             error: function() {
                 console.log('error!')
             },
@@ -14,20 +15,19 @@ $(function () {
         });
     });
 
-    function handleResponse(data) {
+    function artistController(data) {
         var listOfArtists = data.artists.items
         listOfArtists.forEach(function(artist) {
-            var h5 = $('<h5>').text(artist.name);
-            // console.log(h5);
+            var h4 = $('<h4>').text(artist.name);
             if (artist.images.length > 1) {
                 var img = $('<img>').attr('src', artist.images[0].url);
             } else {
                 var img = $('<img>').attr('src', 'avatar.jpg');
             }
-            var li = $('<li>').append(h5);
+            var li = $('<li>').append(h4);
             li.append(img);
             $('#list').append(li);
-            console.log('artist name:', artist.name);
+            // console.log('artist name:', artist.name);
         });
     };
 });
